0e4ea59c9c422cb30606c45339eb5b480a28d9ab
Vera957 <good.answer.e@gmail.com>
2022-06-03 10:36:26 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const imgMarkup = galleryItems
+  .map(
+    (e) =>
+      `
+    <a class="gallery__item" href="${e.original}">
+        <img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+    </a>
+    
+      `
+  )
+  .join("");
+
+gallery.insertAdjacentHTML('beforeend', imgMarkup);
+console.log(imgMarkup)
+//const alt = document.querySelector('gallery__image');
+
+
+
+    
+    let lightbox = new SimpleLightbox(".gallery a", {
+      /* options */
+      captionsData: "alt",
+      captionPosition: "bottom",
+      captions: true,
+      captionDelay: 250,
+    });
+
+    console.log(lightbox.options);
+
+
+
+
+
